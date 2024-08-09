b5a6291102a9374f4d50d34ab0201e54e7c81ea7
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-09 15:33:47 +0900

    init: LogBat 서버 세팅
    - LogBat 스프링 서버를 추가합니다.

---
+++ logbat/src/main/java/info/logbat/LogbatApplication.java
@@ -0,0 +1,13 @@
+package info.logbat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbatApplication.class, args);
+	}
+
+}
