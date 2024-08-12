13d63b16e04aaafa3c81855a453bbc9be8607999
miiiinju1 <gms07073@ynu.ac.kr>
2024-08-12 21:27:29 +0900

    feat: Level enum에 from 메서드 추가
    - level 문자열로부터 enum을 만들 수 있는 from 정적 팩토리 메서드를 추가했습니다.

--- logbat/src/main/java/info/logbat/log/domain/Level.java
+++ logbat/src/main/java/info/logbat/log/domain/Level.java
@@ -2,5 +2,12 @@ package info.logbat.log.domain;
 
 public enum Level {
   ERROR,
-  INFO
+  INFO;
+
+  public static Level from(String level) {
+    if (level == null || level.isBlank()) {
+      throw new IllegalArgumentException("level은 null이거나 빈 문자열일 수 없습니다.");
+    }
+    return Level.valueOf(level.toUpperCase());
+  }
 }
