40a9327cb1125c35f65f74816ada2a753a3308ee
miiiinju1 <gms07073@ynu.ac.kr>
2024-08-12 23:02:52 +0900

    test: LogDataTest에 Displayname 추가
    - LogData VO 테스트 메세지 추가

--- logbat/src/test/java/info/logbat/log/domain/values/LogDataTest.java
+++ logbat/src/test/java/info/logbat/log/domain/values/LogDataTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("LogData VO 테스트")
 class LogDataTest {
 
   @DisplayName("정상적으로 LogData를 만들 수 있다.")
