6815a2ae7c422a74d0a78932087517de467928ef
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-13 17:58:06 +0900

    Feat/#33 Project API (#37)
    * chore: 메서드 파라미터 변경
    
    - ProjectService.updateProjectValues의 파라미터를 변경했습니다.
    
    * feat: 공통 응답 규격 구현
    
    - 공통 응답 규격인 ApiCommonResponse를 구현했습니다.
    
    * feat: Project Controller 구현
    
    - Project API Controller를 구현했습니다. (URI: `/v1/projects`)
      - GET `/{name}`: 프로젝트 조회
      - POST `/`: 프로젝트 생성
      - PUT `/{id}`: 프로젝트 수정
      - DELETE `/{id}`: 프로젝트 제거
    
    * test: Project Controller 테스트
    
    - `@WebMvcTest` 기반 ProjectController 테스트를 진행했습니다.
    
    * chore: ControllerTestSupport 수정
    
    - ProjectService MockBean 이동

--- logbat/src/test/java/info/logbat/domain/common/ControllerTestSupport.java
+++ logbat/src/test/java/info/logbat/domain/common/ControllerTestSupport.java
@@ -3,33 +3,35 @@ package info.logbat.domain.common;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import info.logbat.domain.log.application.LogService;
 import info.logbat.domain.log.presentation.LogController;
+import info.logbat.domain.project.application.ProjectService;
+import info.logbat.domain.project.presentation.ProjectController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-/**
+/*
  * 이후 테스트 하려면 Controller에 대해 controllers에 추가하고, ControllerTestSupport를 상속받아 테스트를 진행하시면 됩니다.
  */
-@WebMvcTest(controllers = {
-    LogController.class
-}
-)
+@WebMvcTest(controllers = {LogController.class, ProjectController.class})
 @ActiveProfiles("test")
 public abstract class ControllerTestSupport {
 
-  @Autowired
-  protected MockMvc mockMvc;
+    @Autowired
+    protected MockMvc mockMvc;
 
-  @Autowired
-  protected ObjectMapper objectMapper;
+    @Autowired
+    protected ObjectMapper objectMapper;
 
-  @MockBean
-  protected LogService logService;
+    @MockBean
+    protected LogService logService;
 
-  /**
-   * 이후 필요한 서비스에 대해 MockBean을 추가하여 테스트를 진행하시면 됩니다.
-   */
+    @MockBean
+    protected ProjectService projectService;
+
+    /*
+     * 이후 필요한 서비스에 대해 MockBean을 추가하여 테스트를 진행하시면 됩니다.
+     */
 
 }
