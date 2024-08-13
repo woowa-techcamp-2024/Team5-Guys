80d650e1f0ec86c7e2eca1fdf64548b4dfa4a2ff
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-13 13:12:03 +0900

    feat: Project 서비스 트랜젝션 추가
    - Project 서비스 메서드들에 대해 `@Transactional`을 적용했습니다.

--- logbat/src/main/java/info/logbat/domain/project/application/ProjectService.java
+++ logbat/src/main/java/info/logbat/domain/project/application/ProjectService.java
@@ -6,9 +6,11 @@ import info.logbat.domain.project.presentation.payload.response.ProjectCommonRes
 import info.logbat.domain.project.repository.ProjectJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ProjectService {
 
     private final ProjectJpaRepository projectRepository;
@@ -18,7 +20,7 @@ public class ProjectService {
         return ProjectCommonResponse.from(project);
     }
 
-
+    @Transactional(readOnly = true)
     public ProjectCommonResponse getProjectByName(String name) {
         Project project = projectRepository.findByName(name)
             .orElseThrow(() -> new IllegalArgumentException("프로젝트를 찾을 수 없습니다."));
