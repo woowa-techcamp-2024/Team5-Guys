4c49e30b582c3f2b7145ddd4e020a3098a245618
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-13 10:21:59 +0900

    Feat/#16 Log 도메인 설계 (#28)
    * feat: 로그 레벨을 나타내는 Level enum 생성
    
    - Level은 ERROR과, INFO를 가집니다.
    
    * feat: 로그 데이터를 담는 LogData VO 생성
    
    - LogData VO 내에서 data에 대한 유효성 검사를 진행합니다.
    
    * test: 로그 데이터에 관한 테스트 작성
    
    - 정상적으로 LogData를 만들 수 있다.
    - 빈 문자열로 LogData를 만들면 예외가 발생한다.
    - null로 LogData를 만들면 예외가 발생한다.
    
    * feat: Level enum에 from 메서드 추가
    
    - level 문자열로부터 enum을 만들 수 있는 from 정적 팩토리 메서드를 추가했습니다.
    
    * test: Level enum에 from 메서드에 관한 테스트 추가
    
    - level 문자열로부터 enum을 만들 수 있는 from 정적 팩토리 메서드에 관한 테스트를 추가했습니다.
    
    * feat: Log 도메인 객체 생성
    
    - Log는 applicationId를 반드시 가져야합니다.
    - Log는 Level을 가져야하며, Level VO 내에서 유효성 검사를 진행합니다.
    - Log는 logData를 가지며, LogData VO 내에서 유효성 검사를 진행합니다.
    - Log는 로그가 생성된 timestamp를 반드시 가져야합니다.
    
    * test: Log 도메인 객체에 대한 단위 테스트 추가
    
    - Log 도메인 객체 생성에 대한 테스트 케이스 구현
      - 새로운 Log 도메인 객체 생성 테스트
      - DB에 저장된 Log 도메인 객체 생성 테스트
    - 정상 케이스 및 예외 케이스(null 값 처리) 테스트 포함
    
    * refactor: domain하위 values, enums 패키지 추가 및 이동
    
    - Level을 enums 하위로 이동
    - LogData를 values 하위로 이동
    - Test코드를 해당 패키지 구조에 맞게 변경
    
    * test: LevelTest에 Dispalyname 추가
    
    * test: LogDataTest에 Displayname 추가
    
    - LogData VO 테스트 메세지 추가
    
    * refactor: LogData VO를 정적 팩토리 메서드를 사용하도록 수정
    
    - 상속에 의한 확장을 방지하고자 VO를 정적 팩토리 메서드를 이용하도록 변경했습니다.
    - LogDataTest의 createLogData 테스트 필드 명을 변경했습니다.
    
    * refactor: Level from메서드 수정
    
    - from 메서드에서 values를 사용하고, 없을 경우 예외를 발생시키도록 수정함
    - level의 upperCase 확인 시 NPE 발생 가능성으로 null check를 상단에 추가
    
    * test: Level enum 테스트 추가
    
    - Level 문자열이 앞 뒤로 공백을 포함하는 경우는 정상적으로 enum을 반환하는 것을 테스트 추가
    - 잘못된 문자열이 들어오는 경우 예외를 발생시키는 테스트 추가

---
+++ logbat/src/main/java/info/logbat/log/domain/Log.java
@@ -0,0 +1,43 @@
+package info.logbat.log.domain;
+
+import info.logbat.log.domain.enums.Level;
+import info.logbat.log.domain.values.LogData;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class Log {
+
+  private final Long logId;
+  private final Long applicationId;
+  private final Level level;
+  private final LogData logData;
+  private final LocalDateTime timestamp;
+
+  public Log(Long applicationId, String level, String logData, LocalDateTime timestamp) {
+    this(null, applicationId, level, logData, timestamp);
+  }
+
+  public Log(Long logId, Long applicationId, String level, String logData,
+      LocalDateTime timestamp) {
+    this.logId = logId;
+    validateApplicationId(applicationId);
+    this.applicationId = applicationId;
+    this.level = Level.from(level);
+    this.logData = LogData.from(logData);
+    validateTimestamp(timestamp);
+    this.timestamp = timestamp;
+  }
+
+  private void validateApplicationId(Long applicationId) {
+    if (applicationId == null) {
+      throw new IllegalArgumentException("applicationId는 null이 될 수 없습니다.");
+    }
+  }
+
+  private void validateTimestamp(LocalDateTime timestamp) {
+    if (timestamp == null) {
+      throw new IllegalArgumentException("timestamp는 null이 될 수 없습니다.");
+    }
+  }
+}
---
+++ logbat/src/test/java/info/logbat/log/domain/enums/LevelTest.java
@@ -0,0 +1,96 @@
+package info.logbat.log.domain.enums;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("로그 Level enum 테스트")
+class LevelTest {
+
+  @DisplayName("빈 문자열의 로그 레벨 문자열을 enum으로 변환하면 예외가 발생한다.")
+  @Test
+  void createLevelWithBlank() {
+    // given
+    String 빈_로그_레벨 = "";
+
+    // when & then
+    assertThatThrownBy(() -> Level.from(빈_로그_레벨))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("level은 null이거나 빈 문자열일 수 없습니다.");
+  }
+
+  @DisplayName("null의 로그 레벨 문자열을 enum으로 변환하면 예외가 발생한다.")
+  @Test
+  void createLevelWithNull() {
+    // given
+    String null_로그_레벨 = null;
+
+    // when & then
+    assertThatThrownBy(() -> Level.from(null_로그_레벨))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("level은 null이거나 빈 문자열일 수 없습니다.");
+  }
+
+  @DisplayName("로그 레벨 문자열이 앞 뒤에 공백을 포함하더라도 enum으로 변환할 수 있다.")
+  @Test
+  void createLevelWithBlankSpace() {
+    // given
+    String 공백_포함_로그_레벨 = "  INFO  ";
+
+    // when
+    Level 로그_레벨 = Level.from(공백_포함_로그_레벨);
+
+    // then
+    assertThat(로그_레벨).isEqualTo(Level.INFO);
+  }
+
+  @DisplayName("올바르지 않은 로그 레벨 문자열을 enum으로 변환하면 예외가 발생한다.")
+  @ParameterizedTest(name = "{index}. 잘못된 입력값: {0}")
+  @MethodSource("invalidLogLevelProvider")
+  void createLevelWithInvalidLevel(String 잘못된_로그_레벨) {
+    // when & then
+    assertThatThrownBy(() -> Level.from(잘못된_로그_레벨))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("level이 올바르지 않습니다.");
+  }
+
+  private static Stream<Arguments> invalidLogLevelProvider() {
+    return Stream.of(
+        Arguments.of("infos"),
+        Arguments.of("infor"),
+        Arguments.of("eror"),
+        Arguments.of("errr"),
+        Arguments.of("err"),
+        Arguments.of("errorr"))
+        ;
+  }
+
+  @DisplayName("다양한 형식의 로그 레벨 문자열을 enum으로 변환할 수 있다.")
+  @ParameterizedTest(name = "{index}. 입력값: {0}, 기대값: {1} enum")
+  @MethodSource("logLevelProvider")
+  void createLevel(String inputLevel, Level expectedLevel) {
+    // when
+    Level 로그_레벨 = Level.from(inputLevel);
+
+    // then
+    assertThat(로그_레벨).isEqualTo(expectedLevel);
+  }
+
+  private static Stream<Arguments> logLevelProvider() {
+    return Stream.of(
+        Arguments.of("info", Level.INFO),
+        Arguments.of("iNfO", Level.INFO),
+        Arguments.of("Info", Level.INFO),
+        Arguments.of("INFO", Level.INFO),
+        Arguments.of("error", Level.ERROR),
+        Arguments.of("ErRoR", Level.ERROR),
+        Arguments.of("ERROR", Level.ERROR)
+    );
+  }
+}
