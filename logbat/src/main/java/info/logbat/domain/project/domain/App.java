f44e18e7a47b610f1a73adaf6d6adc844127a8d8
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-13 16:42:58 +0900

    chore: 인덱싱 추가
    - 토큰 기반 조회 쿼리 성능 향상을 위해 인덱싱을 명시했습니다.

--- logbat/src/main/java/info/logbat/domain/project/domain/App.java
+++ logbat/src/main/java/info/logbat/domain/project/domain/App.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -22,7 +23,9 @@ import org.hibernate.annotations.SoftDelete;
 
 @Entity
 @Getter
-@Table(name = "entities")
+@Table(name = "entities", indexes = {
+    @Index(name = "idx_app_token", columnList = "token")
+})
 @SoftDelete
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class App {
