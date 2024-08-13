07575893f7f76a34581c815fcf826f17aec9a49b
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-13 15:27:36 +0900

    Feat/#18 App 도메인 모델 구현 (#36)
    * feat: AppType Enum 구현
    
    - App Type 구분을 위한 Enum을 구현했습니다.
    - 테스트를 진행했습니다.
    
    * feat: App 도메인 엔티티 구현
    
    - App 도메인 엔티티를 구현했습니다.
      - App 생성 시 전달받은 인자가 없을 경우 예외를 반환하는 검증로직을 추가했습니다.
      - 이에 따른 테스트를 진행했습니다.

---
+++ logbat/src/main/java/info/logbat/domain/project/domain/App.java
@@ -0,0 +1,54 @@
+package info.logbat.domain.project.domain;
+
+import info.logbat.domain.project.domain.enums.AppType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SoftDelete;
+
+@Entity
+@Getter
+@Table(name = "entities")
+@SoftDelete
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class App {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "app_type", nullable = false)
+    private AppType appType;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    private App(Project project, AppType appType) {
+        this.project = Objects.requireNonNull(project, "프로젝트는 필수입니다.");
+        this.appType = Objects.requireNonNull(appType, "앱 타입은 필수입니다.");
+    }
+
+    public static App of(Project project, AppType appType) {
+        return new App(project, appType);
+    }
+
+}
