c4e8a1f1f3f97596adfcec7b3dfec98db4a6d50c
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-13 15:27:15 +0900

    Feat/#31 Log를 저장하는 API 구현 (#34)
    * chore: LogRepository에 잘못 사용한 생성자 변경
    
    - AllArgsConstructor에서 RequiredArgsConstructor로 변경
    
    * feat: CreateLogServiceRequest 레코드 추가
    
    - Service 요청을 담당하는 CreateLogServiceRequest를 추가했습니다ㅏ.
    
    * feat: Log를 저장하는 LogService 추가
    
    - Log를 만들고 저장하는 LogService를 추가했습니다.
    - AppId 검증 로직이 이후에 추가되어야 합니다.
    
    * test: Log를 저장하는 LogService에 대한 테스트 추가
    
    - Log를 만들고 저장하는 LogService에 대한 테스트를 추가했습니다.
    - AppId 검증 로직에 대한 테스트가 추가되어야 합니다.
    
    * feat: Controller의 요청을 받기 위해 CreateLogRequest를 추가했습니다.
    
    - logLevel은 `@NotBlank`를 통해 검증했습니다.
    - logData를 `@NotBlank`를 통해 검증했습니다.
    - LocalDateTime을 `@NotNull`를 통해 검증했습니다.
    
    * feat: 컨트롤러 Request에서 CreateLogServiceRequest변환하는 정적 팩토리 메서드 추가
    
    - Header에서 받아온 applicationId와 Controller Request를 파라미터로 받아서 만들도록 구현
    
    * refactor: CreateLogRequest를 레코드를 사용하도록 변경
    
    - CreateLogRequest를 레코드로 변경했습니다.
    - CreateLogServiceRequest의 of메서드를 파라미터 변경
    
    * config: build.gradle에 validation 추가
    
    * feat: LogController 추가
    
    - applicationId에 대해 Positive, NotNull Validation 추가
    - CreateLogRequest에 대해 Validation 추가
    
    * test: Cntroller 테스트 환경 통합을 위해 ControllerTestSupport 추가
    
    * test: LogController에 대한 단위 테스트 추가
    
    - 유효한 입력 값으로 로그 생성 성공 테스트.
    - app_id 누락 시 400 Bad Request 반환 테스트.
    - 음수 app_id에 대한 400 Bad Request 반환 테스트.
    - logLevel 및 logData 필드에 대한 null 및 빈 문자열 입력 시 400 Bad Request 반환 테스트.
    - timestamp 누락 시 400 Bad Request 반환 테스트.
    
    * refactor: Transactional import를 jakarta에서 springframework로 변경
    
    * refactor: logController에서 prefix logs는 RequestMapping을 사용하도록 수정
    
    - `@RequestMapping("/logs")`와 `@PostMapping`을 사용하게 수정
    
    * feat: Validation에서 message 추가하도록 변경
    
    - LogController의  applicationId에 메세지 추가
    - CreateLogRequest의 Validation에 메세지 추가
    
    * feat: Log를 생성하는 of 정적 팩터리 메서드 추가
    
    - LogService에서 Log를 생성 시 of 팩터리 메서드를 이용하도록 변경

---
+++ logbat/src/main/java/info/logbat/domain/log/application/payload/request/CreateLogServiceRequest.java
@@ -0,0 +1,21 @@
+package info.logbat.domain.log.application.payload.request;
+
+import info.logbat.domain.log.presentation.payload.request.CreateLogRequest;
+import java.time.LocalDateTime;
+
+public record CreateLogServiceRequest(
+    Long applicationId,
+    String logLevel,
+    String logData,
+    LocalDateTime timestamp
+) {
+
+  public static CreateLogServiceRequest of(Long applicationId, CreateLogRequest request) {
+    return new CreateLogServiceRequest(
+        applicationId,
+        request.logLevel(),
+        request.logData(),
+        request.timestamp()
+    );
+  }
+}
