9507f46fcd1512b4177058bd4f57a355079ba789
miiiinju1 <gms07073@ynu.ac.kr>
2024-08-13 12:39:31 +0900

    test: Repository Test 시 Test Profile을 사용하도록 변경

--- logbat/src/test/java/info/logbat/domain/log/repository/LogRepositoryTest.java
+++ logbat/src/test/java/info/logbat/domain/log/repository/LogRepositoryTest.java
@@ -11,9 +11,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 @DisplayName("LogRepository 테스트")
 class LogRepositoryTest {
 
