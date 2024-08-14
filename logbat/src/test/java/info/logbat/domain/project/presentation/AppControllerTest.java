73298826e0ce40a3e301fc3da072ceed4df62941
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-14 23:22:54 +0900

    Feat/#48 App API 구현 (#50)
    * feat: App List 조회 기능 추가 - 서비스
    
    - 서비스에서 Project Id를 통해 App들을 조회할 수 있는 기능을 구현했습니다.
    - 이에 따른 테스트를 추가했습니다.
    
    * feat: App 조회 구현 - 컨트롤러
    
    - App 조회에 대한 API를 구현했습니다.
      - `/v1/projects/apps/{projectId}`: 프로젝트 ID로 앱 목록을 조회
      - `/v1/projects/apps/info/{id}`: 앱 ID로 앱 정보 조회
    
    * refactor: App 생성 로직 리팩토링 - 서비스
    
    - createApp 파라미터 타입을 AppType에서 String으로 변경했습니다.
    - 이에 따른 테스트를 수정 및 추가했습니다.
    
    * feat: App 생성 구현 - 컨트롤러
    
    - App 생성에 대한 API를 구현했습니다.
      - POST `/v1/projects/apps`: Body로 projectId와 AppType을 전달받아서 App을 생성합니다.
    
    * feat: App 제거 구현 - 컨트롤러
    
    - App 제거에 대한 API를 구현했습니다.
      - DELETE `/v1/projects/{projectId}/{appId}`: App을 삭제할 수 있습니다.

---
+++ logbat/src/test/java/info/logbat/domain/project/presentation/AppControllerTest.java
@@ -0,0 +1,142 @@
+package info.logbat.domain.project.presentation;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import info.logbat.domain.common.ControllerTestSupport;
+import info.logbat.domain.project.domain.enums.AppType;
+import info.logbat.domain.project.presentation.payload.response.AppCommonResponse;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("AppController는")
+class AppControllerTest extends ControllerTestSupport {
+
+    private final AppCommonResponse expectedAppCommonResponse = mock(AppCommonResponse.class);
+    private final Long expectedId = 1L;
+    private final Long expectedProjectId = 1L;
+    private final AppType expectedAppType = AppType.JAVA;
+    private final UUID expectedToken = UUID.randomUUID();
+    private final LocalDateTime expectedCreatedAt = LocalDateTime.now();
+
+    @BeforeEach
+    void init() {
+        given(expectedAppCommonResponse.id()).willReturn(expectedId);
+        given(expectedAppCommonResponse.projectId()).willReturn(expectedProjectId);
+        given(expectedAppCommonResponse.appType()).willReturn(expectedAppType.name());
+        given(expectedAppCommonResponse.token()).willReturn(expectedToken.toString());
+        given(expectedAppCommonResponse.createdAt()).willReturn(expectedCreatedAt);
+    }
+
+    @Nested
+    @DisplayName("GET에 대해")
+    class describeGetApps {
+
+        @Test
+        @DisplayName("/v1/projects/apps/{projectId} 요청시 프로젝트 ID로 앱 목록을 조회할 수 있다.")
+        void willReturnAppList() throws Exception {
+            // Arrange
+            given(appService.getAppsByProjectId(expectedProjectId)).willReturn(
+                List.of(expectedAppCommonResponse));
+            MockHttpServletRequestBuilder get = get("/v1/projects/apps/{projectId}",
+                expectedProjectId);
+            // Act & Assert
+            mockMvc.perform(get)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andExpect(jsonPath("$.message").value("Success"))
+                .andExpect(jsonPath("$.data[0].id").value(expectedId))
+                .andExpect(jsonPath("$.data[0].projectId").value(expectedProjectId))
+                .andExpect(jsonPath("$.data[0].appType").value(expectedAppType.name()))
+                .andExpect(jsonPath("$.data[0].token").value(expectedToken.toString()))
+                .andExpect(jsonPath("$.data[0].createdAt").value(expectedCreatedAt.toString()));
+        }
+
+        @Test
+        @DisplayName("/v1/projects/apps/info/{id} 요청시 앱 ID로 앱 정보를 조회할 수 있다.")
+        void willReturnAppInformation() throws Exception {
+            // Arrange
+            given(appService.getAppById(expectedId)).willReturn(expectedAppCommonResponse);
+            MockHttpServletRequestBuilder get = get("/v1/projects/apps/info/{id}", expectedId);
+            // Act & Assert
+            mockMvc.perform(get)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andExpect(jsonPath("$.message").value("Success"))
+                .andExpect(jsonPath("$.data.id").value(expectedId))
+                .andExpect(jsonPath("$.data.projectId").value(expectedProjectId))
+                .andExpect(jsonPath("$.data.appType").value(expectedAppType.name()))
+                .andExpect(jsonPath("$.data.token").value(expectedToken.toString()))
+                .andExpect(jsonPath("$.data.createdAt").value(expectedCreatedAt.toString()));
+        }
+
+    }
+
+    @Nested
+    @DisplayName("POST에 대해")
+    class describePostApp {
+
+        @Test
+        @DisplayName("/v1/projects/apps 요청시 앱을 생성할 수 있다.")
+        void willCreateApp() throws Exception {
+            // Arrange
+            given(appService.createApp(expectedProjectId, expectedAppType.name()))
+                .willReturn(expectedAppCommonResponse);
+            MockHttpServletRequestBuilder post = post("/v1/projects/apps")
+                .contentType("application/json")
+                .content("""
+                    {
+                        "projectId": 1,
+                        "appType": "JAVA"
+                    }
+                    """);
+            // Act & Assert
+            mockMvc.perform(post)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andExpect(jsonPath("$.message").value("Success"))
+                .andExpect(jsonPath("$.data.id").value(expectedId))
+                .andExpect(jsonPath("$.data.projectId").value(expectedProjectId))
+                .andExpect(jsonPath("$.data.appType").value(expectedAppType.name()))
+                .andExpect(jsonPath("$.data.token").value(expectedToken.toString()))
+                .andExpect(jsonPath("$.data.createdAt").value(expectedCreatedAt.toString()));
+        }
+
+    }
+
+    @Nested
+    @DisplayName("DELETE에 대해")
+    class describeDeleteApp {
+
+        @Test
+        @DisplayName("/v1/projects/apps/{projectId}/{appId} 요청시 프로젝트 ID와 앱 ID로 앱을 삭제할 수 있다.")
+        void willDeleteApp() throws Exception {
+            // Arrange
+            given(appService.deleteApp(expectedProjectId, expectedId)).willReturn(expectedId);
+            MockHttpServletRequestBuilder delete = delete("/v1/projects/apps/{projectId}/{appId}",
+                expectedProjectId, expectedId);
+            // Act & Assert
+            mockMvc.perform(delete)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusCode").value(200))
+                .andExpect(jsonPath("$.message").value("Success"))
+                .andExpect(jsonPath("$.data").value(expectedId));
+        }
+
+    }
+
+}
