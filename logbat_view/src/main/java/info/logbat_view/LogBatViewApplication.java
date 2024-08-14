6d946c110e9f43ce2cf6704241cdc443c0b48648
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-14 22:53:31 +0900

    chore: 사용하지 않는 테스트 삭제 및 설정
    - 사용하지 않는 테스트를 삭제했습니다.
    - LogbatViewApplication -> LogBatViewApplication 클래스명 변경

--- logbat_view/src/main/java/info/logbat_view/LogBatViewApplication.java
+++ logbat_view/src/main/java/info/logbat_view/LogBatViewApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LogbatViewApplication {
+public class LogBatViewApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LogbatViewApplication.class, args);
+		SpringApplication.run(LogBatViewApplication.class, args);
 	}
 
 }
