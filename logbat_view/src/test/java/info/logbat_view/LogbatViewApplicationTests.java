6394f3f0e9523270390d77435468003a277c1c18
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-14 22:44:03 +0900

    init: 프로젝트 세팅
    - view server 세팅을 진행합니다.

---
+++ logbat_view/src/test/java/info/logbat_view/LogbatViewApplicationTests.java
@@ -0,0 +1,13 @@
+package info.logbat_view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbatViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
