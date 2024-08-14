a07a86c7202ec03a1b6d56c7754e2ab5758f35f3
miiiinju1 <gms07073@ynu.ac.kr>
2024-08-14 23:27:42 +0900

    test: Displayname 추가

--- logbat/src/test/java/info/logbat/common/util/UUIDUtilTest.java
+++ logbat/src/test/java/info/logbat/common/util/UUIDUtilTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@DisplayName("UUIDUtil에서 ")
 public class UUIDUtilTest {
 
   @ParameterizedTest
