e2aa389ba4fb81c3ebeda8e50c1b8fdefb2e3604
miiiinju1 <gms07073@ynu.ac.kr>
2024-08-15 17:48:00 +0900

    feat: Thread별로 count를 수집하는 DivideCountTestService 구현
    - ThreadLocal로 각 스레드 내에서 count 증가하도록 구현
    - 메인 카운터 업데이트 횟수를 줄이기 위한 버퍼 메커니즘 도입
    - 활성 Count 스레드 추적을 위해 Thread를 키로 사용하는 HashMap 사용
    - 카운터 연산의 스레드 안전성 보장
    - ThreadLocal 인스턴스를 유지하도록 리셋 작업 최적화
    - 정확한 카운트 검색을 위한 플러시 메커니즘의 스레드 안전성 보장
    - Counter 인스턴스가 스레드 수에 의존하기에 항상 보존하도록 설정, 활성 HashMap은 초기화하도록 결정

---
+++ logbat/src/main/java/info/logbat/dev/service/DivideCountTestService.java
@@ -0,0 +1,124 @@
+package info.logbat.dev.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Primary
+@Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DivideCountTestService implements CountTestService {
+
+  private final ThreadLocal<Counter> localCounter = ThreadLocal.withInitial(Counter::new);
+
+  private static final Long BUFFER_SIZE = 1000L;
+
+  private final AtomicLong successCount = new AtomicLong(0L);
+  private final AtomicLong errorCount = new AtomicLong(0L);
+
+  // ThreadLocal -> Counter 인스턴스
+  //                  ^
+  //                 /
+  // Map.get(Thread)
+
+  // Key가 Thread.currentThread()이기 때문에 경쟁상태가 발생하지 않음
+  private final Map<Thread, Counter> activeThreads = new HashMap<>(150);
+
+  public void increaseSuccessCount() {
+    Counter counter = localCounter.get();
+
+    // Thread 내의 Counter에 증가연산을 수행하여, 경쟁상태가 발생하지 않음
+    counter.increaseSuccessCount();
+    activeThreads.putIfAbsent(Thread.currentThread(), counter);
+
+    if (counter.getSuccessCount() >= BUFFER_SIZE) {
+      successCount.addAndGet(counter.getSuccessCount());
+      counter.resetIncreaseCount();
+    }
+  }
+
+  public void increaseErrorCount() {
+    Counter counter = localCounter.get();
+
+    // Thread 내의 Counter에 증가연산을 수행하여, 경쟁상태가 발생하지 않음
+    counter.increaseErrorCount();
+    activeThreads.putIfAbsent(Thread.currentThread(), counter);
+
+    if (counter.getErrorCount() >= BUFFER_SIZE) {
+      errorCount.addAndGet(counter.getErrorCount());
+      counter.resetErrorCount();
+    }
+  }
+
+  public long getSuccessCount() {
+    flushAllThreadLocals();
+    return successCount.get();
+  }
+
+  public long getErrorCount() {
+    flushAllThreadLocals();
+    return errorCount.get();
+  }
+
+  public void reset() {
+    resetAllThreadLocals();
+    successCount.set(0L);
+    errorCount.set(0L);
+  }
+
+  // 중복 집계되는 경우를 방지하기 위해, flush 시 Counter 객체에 대해서 동기화 처리
+  private void flushAllThreadLocals() {
+    activeThreads.values().forEach(counter -> {
+      synchronized (counter) {
+        successCount.addAndGet(counter.getSuccessCount());
+        errorCount.addAndGet(counter.getErrorCount());
+        counter.resetIncreaseCount();
+        counter.resetErrorCount();
+      }
+    });
+  }
+
+  private void resetAllThreadLocals() {
+    activeThreads.values().forEach(counter -> {
+      counter.resetIncreaseCount();
+      counter.resetErrorCount();
+    });
+
+    activeThreads.clear();
+  }
+
+  static class Counter {
+
+    private long successCount;
+    private long errorCount;
+
+    public void increaseSuccessCount() {
+      successCount++;
+    }
+
+    public void increaseErrorCount() {
+      errorCount++;
+    }
+
+    public long getSuccessCount() {
+      return successCount;
+    }
+
+    public long getErrorCount() {
+      return errorCount;
+    }
+
+    public void resetIncreaseCount() {
+      successCount = 0L;
+    }
+
+    public void resetErrorCount() {
+      errorCount = 0L;
+    }
+
+  }
+}
