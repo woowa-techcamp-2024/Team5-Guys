22348fc08121aa448036e43c5cd638cb21629b97
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-15 14:28:25 +0900

    feat: Log 조회 기능 구현 - 서비스
    - AppKey 기반 조회 이후 결과를 반환하는 서비스 로직을 구현했습니다.
    - 이에 따른 테스트를 추가했습니다.

---
+++ logbat_view/src/main/java/info/logbat_view/domain/log/application/LogViewService.java
@@ -0,0 +1,19 @@
+package info.logbat_view.domain.log.application;
+
+import info.logbat_view.domain.log.domain.service.LogService;
+import info.logbat_view.domain.log.presentation.payload.response.LogCommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+@RequiredArgsConstructor
+public class LogViewService {
+
+    private final LogService logService;
+
+    public Flux<LogCommonResponse> findLogs(String appKey) {
+        return logService.findLogsByAppKey(appKey).map(LogCommonResponse::from);
+    }
+
+}
---
+++ logbat_view/src/test/java/info/logbat_view/domain/log/application/LogViewServiceTest.java
@@ -0,0 +1,60 @@
+package info.logbat_view.domain.log.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import info.logbat_view.domain.log.domain.Log;
+import info.logbat_view.domain.log.domain.LogData;
+import info.logbat_view.domain.log.domain.enums.LogLevel;
+import info.logbat_view.domain.log.domain.service.LogService;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("LogViewService는")
+class LogViewServiceTest {
+
+    @InjectMocks
+    private LogViewService logViewService;
+
+    @Mock
+    private LogService logService;
+
+    private final Long expectedId = 1L;
+    private final UUID expectedAppKey = UUID.randomUUID();
+    private final LogLevel expectedLevel = LogLevel.INFO;
+    private final String expectedData = "data";
+    private final LocalDateTime expectedTimestamp = LocalDateTime.of(2024, 8, 15, 12, 0, 0, 0);
+    private final Log expectedLog = Log.from(
+        new LogData(expectedId, expectedAppKey.toString(), expectedLevel.name(), expectedData,
+            expectedTimestamp));
+
+
+    @Test
+    @DisplayName("appkey로 로그를 조회한다.")
+    void findLogsByAppKey() {
+        // Arrange
+        given(logService.findLogsByAppKey(any(String.class))).willReturn(Flux.just(expectedLog));
+        // Act & Assert
+        logViewService.findLogs(expectedAppKey.toString())
+            .as(StepVerifier::create)
+            .expectNextMatches(response -> {
+                assertThat(response)
+                    .extracting("id", "level", "data", "timestamp")
+                    .containsExactly(expectedId, expectedLevel.name(), expectedData,
+                        expectedTimestamp);
+                return true;
+            })
+            .verifyComplete();
+    }
+
+}
