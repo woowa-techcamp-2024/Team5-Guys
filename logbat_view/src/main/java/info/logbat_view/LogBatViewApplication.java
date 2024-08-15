8a45723fbc2e4ce09c5120f2c3926dddb9eb66ec
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-15 09:08:50 +0900

    Feat/#55 Log View 서버 세팅 (#56)
    * init: 프로젝트 세팅
    
    - view server 세팅을 진행합니다.
    
    * chore: 사용하지 않는 테스트 삭제 및 설정
    
    - 사용하지 않는 테스트를 삭제했습니다.
    - LogbatViewApplication -> LogBatViewApplication 클래스명 변경

---
+++ logbat_view/src/main/java/info/logbat_view/LogBatViewApplication.java
@@ -0,0 +1,13 @@
+package info.logbat_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogBatViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogBatViewApplication.class, args);
+	}
+
+}
