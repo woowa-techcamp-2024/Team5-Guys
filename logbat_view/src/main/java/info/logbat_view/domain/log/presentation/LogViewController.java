fb12431d1b0a2557856bf8923e01f084a50a85c4
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-16 19:45:19 +0900

    feat: body로 처리하기 위한 LogCursorRequest 구현
    - LogCursorRequest 구현

--- logbat_view/src/main/java/info/logbat_view/domain/log/presentation/LogViewController.java
+++ logbat_view/src/main/java/info/logbat_view/domain/log/presentation/LogViewController.java
@@ -2,12 +2,13 @@ package info.logbat_view.domain.log.presentation;
 
 import info.logbat_view.common.payload.CursorPaginationResult;
 import info.logbat_view.domain.log.application.LogViewService;
+import info.logbat_view.domain.log.presentation.payload.request.LogCursorRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -20,13 +21,12 @@ public class LogViewController {
     private final LogViewService logViewService;
 
     @GetMapping("/{appKey}")
-    public Mono<CursorPaginationResult> getLogs(@PathVariable String appKey,
-        @RequestParam(name = "cursor", defaultValue = "-1", required = false) Long cursor,
-        @RequestParam(name = "size", defaultValue = "10", required = false) Integer size) {
-        log.info("appKey: {}, cursor: {}, size: {}", appKey, cursor, size);
-        return logViewService.findLogs(appKey, cursor, size + 1)
+    public Mono<CursorPaginationResult> getLogs(@PathVariable String appKey, @RequestBody
+    LogCursorRequest request) {
+        log.info("appKey: {}, cursor: {}, size: {}", appKey, request.cursor(), request.size());
+        return logViewService.findLogs(appKey, request.cursor(), request.size() + 1)
             .collectList()
-            .map(data -> CursorPaginationResult.of(data, size));
+            .map(data -> CursorPaginationResult.of(data, request.size()));
     }
 
 }
