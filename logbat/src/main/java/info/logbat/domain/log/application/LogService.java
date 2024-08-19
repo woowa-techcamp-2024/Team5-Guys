09d56f3f4aff3ea3b4664a8631bb661e75c38bc6
miiiinju1 <gms07073@ynu.ac.kr>
2024-08-19 13:46:29 +0900

    feat: Log를 저장할 때 appKey가 존재하는 지 확인하도록 수정

--- logbat/src/main/java/info/logbat/domain/log/application/LogService.java
+++ logbat/src/main/java/info/logbat/domain/log/application/LogService.java
@@ -3,7 +3,9 @@ package info.logbat.domain.log.application;
 import info.logbat.domain.log.application.payload.request.CreateLogServiceRequest;
 import info.logbat.domain.log.domain.Log;
 import info.logbat.domain.log.repository.LogRepository;
+import info.logbat.domain.project.repository.AppJpaRepository;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +14,30 @@ import org.springframework.stereotype.Service;
 public class LogService {
 
   private final LogRepository logRepository;
+  private final AppJpaRepository appJpaRepository;
 
   public long saveLog(CreateLogServiceRequest request) {
     String appKey = request.appKey();
     String level = request.level();
     String data = request.data();
     LocalDateTime timestamp = request.timestamp();
-    // TODO Log 저장 전 Application ID 체크 로직 추가 필요
+
+    UUID appKeyUuid = getAppKey(appKey);
+
+    if (!appJpaRepository.existsByToken(appKeyUuid)) {
+      throw new IllegalArgumentException("존재하지 않는 Application Key 입니다.");
+    }
 
     Log log = Log.of(appKey, level, data, timestamp);
 
     return logRepository.save(log);
+  }
+
+  private UUID getAppKey(String appKey) {
+    try {
+      return UUID.fromString(appKey);
+    } catch (IllegalArgumentException e) {
+      throw new IllegalArgumentException("잘못된 형식의 Application Key 입니다.");
+    }
   }
 }
