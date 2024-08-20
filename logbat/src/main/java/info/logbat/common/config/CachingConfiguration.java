13c745d67abd8f4d0a7bd1f723eab37354cf6d64
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-20 15:33:07 +0900

    feat: CachingConfiguration 구현
    - 캐싱을 위한 CachingConfiguration 구현을 진행했습니다.

---
+++ logbat/src/main/java/info/logbat/common/config/CachingConfiguration.java
@@ -0,0 +1,26 @@
+package info.logbat.common.config;
+
+import java.util.List;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfiguration {
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(
+            List.of(
+                new ConcurrentMapCache("app_key"),
+                new ConcurrentMapCache("app_id")
+            )
+        );
+        return cacheManager;
+    }
+}
