9e24e2f45a0e8a74200f5c931524594a6a2582b3
luizy <luizy991212@gmail.com>
2024-08-20 19:42:35 +0900

    config: init logbat_meta project

---
+++ logbat_meta/src/main/java/info/logbat_meta/LogbatMetaApplication.java
@@ -0,0 +1,13 @@
+package info.logbat_meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbatMetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbatMetaApplication.class, args);
+    }
+
+}
---
+++ logbat_meta/src/test/java/info/logbat_meta/LogbatMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package info.logbat_meta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbatMetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
