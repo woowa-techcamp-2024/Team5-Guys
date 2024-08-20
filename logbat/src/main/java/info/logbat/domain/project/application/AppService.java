d063f6283876da647b1560a06f22aa5adaba1900
luizy <luizy991212@gmail.com>
2024-08-20 22:49:47 +0900

    refactor: JPA 관련 코드 제거

--- logbat/src/main/java/info/logbat/domain/project/application/AppService.java
+++ logbat/src/main/java/info/logbat/domain/project/application/AppService.java
@@ -5,17 +5,14 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 @CacheConfig(cacheNames = {"app"})
 public class AppService {
 
     private static final String APP_NOT_FOUND_MESSAGE = "앱을 찾을 수 없습니다.";
 
-    @Transactional(readOnly = true)
     @Cacheable(key = "#token")
     public Long getAppIdByToken(String token) {
         return 1L;
