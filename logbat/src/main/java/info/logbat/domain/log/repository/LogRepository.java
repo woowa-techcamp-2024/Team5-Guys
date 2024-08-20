e7520240c0168bfac7d6f6d97959266c10a4cf04
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-20 14:20:13 +0900

    Feat/#89 Log Insert 시 비동기 Bulk Insert 하도록 구현 (#90)
    * refactor: 추가 비동기 LogRepository 구현을 위한 인터페이스 분리
    
    - 기존 LogRepository를 SynchronousLogRepository로 변경 후 인터페이스 구현하도록
    - 테스트코드 클래스명 수정
    - 새로운 비동기 LogRepository를 AsyncLogRepository 클래스 추가
    
    * feat: AsyncLogRepository 구현
    
    - 비동기 로그 저장을 위한 LinkedBlockingQueue 도입
    - Leader-Follower 패턴을 사용한 로그 처리 구현
      - Leader 스레드: 로그 수집 및 벌크 작업 준비
      - Follower 스레드 풀: 데이터베이스에 로그 저장
    
    * refactor: AsyncLogRepository에서 비동기 처리 로직을 AsyncLogProcessor로 분리
    
    - 코드의 관심사를 분리
    	- AsyncLogRepository는 로그 저장 및 조회 인터페이스에 집중
    	- AsyncLogProcessor는 비동기 로그 처리 로직을 전담
    
    * test: AsyncLogRepository에 대한 단위 테스트 추가
    
    - save 메소드에서 AsyncLogProcessor에 로그가 올바르게 전달되는만 검증하도록 구현
    
    * test: AsyncLogProcessor의 단일 및 벌크 로그 처리 기능 테스트 추가
    
    - init에서 saveFunction을 CountDownLatch로 mocking하여 간접 테스트를 진행했습니다.
       - 처리된 log의 count를 세고, latch를 countdown하는 saveFunction mocking
    
    - 단일 로그 제출 및 처리 테스트
       - 하나의 로그가 정확히 처리되는지 확인
       - 비동기 처리 완료를 CountDownLatch로 대기하여 검증
    - 벌크 로그 제출 및 처리 테스트
       - 기본 벌크 크기(100)보다 많은 150개의 로그를 제출하여 검증
       - 모든 로그가 처리되는지, 그리고 최소 2번의 벌크 처리가
         이루어지는지 확인
    
    * test: 로그 저장 후 id를 반환하지 않게 바뀐 점 수정
    
    - logServiceTest에서는 로그를 저장하는 서비스에서 예외 발생 여부만 검증하도록 수정
    
    * test: SynchronousLogRepositoryTest 의존성 구체화
    
    - AsyncLogRepository에 Primary 어노테이션이 추가된 점으로 SynchronousLogRepositoryTest에서 구체로 의존성 주입 받도록 수정함
    
    * fix: TimeUnit MilliSeconds로 변경
    
    - 잘못 설정 된 2000초에서 2초로 변경
    
    * refactor: feat/#89 변경사항 반영
    
    - appKey를 저장하는 로직에서 appId를 저장하는 로직으로 리팩토링했습니다.
    
    * fix: 히카리풀 10개로 통제함에 따라 followerThread 수 8개로 고정
    
    * fix: followerThread 수 10개로 변경
    
    * feat: 데이터베이스 커넥션 풀에 따라 FollerExcutor 사이즈를 설정하도록 변경
    
    * fix: 테스트 모킹 수정
    
    * refactor: 로직 최적화
    
    - timeout과 bulkSize를 생성자 매개변수로 변경
    - CompletableFuture를 사용한 비동기 초기화 도입
    - 불필요한 null 체크 제거 및 Objects.requireNonNullElse 사용
    - 생성자의 예외 처리 개선
    
    * config: public application.yml 추가
    
    - application.properties 제거
    
    * refactor: new ArrayList<>()로직을 poll 검증 이후로 변경
    
    ---------
    
    Co-authored-by: CodingLuizy <luizy991212@gmail.com>
    Co-authored-by: KyungMin Lee <hello.tidavid@gmail.com>

--- logbat/src/main/java/info/logbat/domain/log/repository/LogRepository.java
+++ logbat/src/main/java/info/logbat/domain/log/repository/LogRepository.java
@@ -1,64 +1,11 @@
 package info.logbat.domain.log.repository;
 
 import info.logbat.domain.log.domain.Log;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
-import java.sql.Timestamp;
 import java.util.Optional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Repository;
 
-@Repository
-@RequiredArgsConstructor
-public class LogRepository {
+public interface LogRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+  long save(Log log);
 
-    public long save(Log log) {
-        String sql = "INSERT INTO logs (app_Id, level, data, timestamp) VALUES (?, ?, ?, ?)";
-
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql,
-                Statement.RETURN_GENERATED_KEYS);
-            ps.setLong(1, log.getAppId());
-            ps.setInt(2, log.getLevel().ordinal());
-            ps.setString(3, log.getData().getValue());
-            ps.setTimestamp(4, Timestamp.valueOf(log.getTimestamp()));
-            return ps;
-        }, keyHolder);
-
-        return Optional.ofNullable(keyHolder.getKey())
-            .map(Number::longValue)
-            .orElseThrow(() -> new IllegalStateException("로그 저장에 실패했습니다."));
-    }
-
-    public Optional<Log> findById(Long id) {
-        String sql = "SELECT * FROM logs WHERE id = ?";
-
-        try {
-            return Optional.ofNullable(
-                jdbcTemplate.queryForObject(
-                    sql,
-                    LOG_ROW_MAPPER,
-                    id
-                ));
-        } catch (EmptyResultDataAccessException e) {
-            return Optional.empty();
-        }
-    }
-
-    private static final RowMapper<Log> LOG_ROW_MAPPER = (rs, rowNum) -> new Log(
-        rs.getLong("id"),
-        rs.getLong("app_id"),
-        rs.getInt("level"),
-        rs.getString("data"),
-        rs.getTimestamp("timestamp").toLocalDateTime()
-    );
+  Optional<Log> findById(Long logId);
 }
---
+++ logbat/src/test/java/info/logbat/domain/log/repository/AsyncLogProcessorTest.java
@@ -0,0 +1,87 @@
+package info.logbat.domain.log.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import com.zaxxer.hikari.HikariDataSource;
+import info.logbat.domain.log.domain.Log;
+import java.time.LocalDateTime;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@DisplayName("AsyncLogProcessor 기능 테스트")
+class AsyncLogProcessorTest {
+
+    private final JdbcTemplate jdbcTemplate = Mockito.mock(JdbcTemplate.class);
+
+    private final HikariDataSource hikariDataSource = Mockito.mock(HikariDataSource.class);
+
+    private AsyncLogProcessor asyncLogProcessor;
+    private AtomicInteger processedLogCount;
+    private CountDownLatch latch;
+
+    @BeforeEach
+    void setUp() {
+        // HikariDataSource 모킹
+        when(jdbcTemplate.getDataSource()).thenReturn(hikariDataSource);
+        when(hikariDataSource.getMaximumPoolSize()).thenReturn(10); // 원하는 풀 사이즈 설정
+
+        asyncLogProcessor = new AsyncLogProcessor(2000L, 100, jdbcTemplate);
+        processedLogCount = new AtomicInteger(0);
+    }
+
+    @DisplayName("단일 로그 제출 및 처리 확인")
+    @Test
+    void testSubmitSingleLog() throws InterruptedException {
+        // given
+        latch = new CountDownLatch(1);
+        asyncLogProcessor.init(logs -> {
+            processedLogCount.addAndGet(logs.size());
+            latch.countDown();
+        });
+        Long 앱_ID = 1L;
+
+        Log log = new Log(1L, 앱_ID, 0, "Test log",
+            LocalDateTime.of(2021, 1, 1, 0, 0, 0));
+
+        // when
+        asyncLogProcessor.submitLog(log);
+
+        // then
+        assertThat(latch.await(5, TimeUnit.SECONDS)).isTrue();
+        assertThat(processedLogCount.get())
+            .isEqualTo(1);
+    }
+
+    @DisplayName("벌크 로그 제출 및 처리 확인")
+    @Test
+    void testSubmitBulkLogs() throws InterruptedException {
+        // given
+        int logCount = 150; // DEFAULT_BULK_SIZE(100)보다 큰 값
+        latch = new CountDownLatch(2); // 최소 2번의 처리를 기대
+        asyncLogProcessor.init(logs -> {
+            processedLogCount.addAndGet(logs.size());
+            latch.countDown();
+        });
+
+        Long 앱_ID = 1L;
+
+        // when
+        for (int i = 0; i < logCount; i++) {
+            asyncLogProcessor.submitLog(
+                new Log((long) i, 앱_ID, 0, "Test log " + i,
+                    LocalDateTime.of(2021, 1, 1, 0, 0, 0)));
+        }
+
+        // then
+        assertThat(latch.await(5, TimeUnit.SECONDS)).isTrue();
+        assertThat(processedLogCount.get()).isEqualTo(logCount);
+    }
+
+}
