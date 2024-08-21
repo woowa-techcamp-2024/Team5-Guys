9a86e8f444a7ab8dde98ff2fe68149fa627196a5
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-21 23:36:57 +0900

    refactor: 로직 리팩토링 및 TODO 주석 작성
    - sendFunction을 이후 활용할 수 있도록 필드에 지정

--- sdk/java/logbat-sdk/src/main/java/info/logbat/infrastructure/LogProcessScheduler.java
+++ sdk/java/logbat-sdk/src/main/java/info/logbat/infrastructure/LogProcessScheduler.java
@@ -1,6 +1,7 @@
 package info.logbat.infrastructure;
 
 import info.logbat.infrastructure.payload.LogSendRequest;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -9,9 +10,12 @@ import java.util.function.Consumer;
 
 public class LogProcessScheduler {
 
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     private static final Integer DEFAULT_BULK_SIZE = 100;
     private static final Integer DEFAULT_INTERVAL = 2000;
+
+    private final Consumer<List<LogSendRequest>> sendFunction;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
 
     /**
      * LogProcessScheduler는 주기적으로 LogBuffer에 저장된 로그를 읽어와 전송하는 스케줄러입니다. logBuffer에 로그가 없을 경우 아무 동작도
@@ -21,15 +25,19 @@ public class LogProcessScheduler {
      * @param logBuffer
      */
     public LogProcessScheduler(Consumer<List<LogSendRequest>> sendFunction, LogBuffer logBuffer) {
+        this.sendFunction = sendFunction;
         scheduler.scheduleAtFixedRate(
             () -> {
+                // catch block에서 logs를 잡아 retryQueue에 넣는 로직을 구현하기 위해 try catch block 이전에 선언했습니다.
+                List<LogSendRequest> logs = new ArrayList<>();
                 try {
-                    final List<LogSendRequest> logs = logBuffer.getLogs(DEFAULT_BULK_SIZE);
-                    if (logs == null || logs.isEmpty()) {
+                    logs.addAll(logBuffer.getLogs(DEFAULT_BULK_SIZE));
+                    if (logs.isEmpty()) {
                         return;
                     }
-                    sendFunction.accept(logs);
+                    this.sendFunction.accept(logs);
                 } catch (Throwable e) {
+                    // TODO retryQueue에 저장하고 재처리 하도록 구현 필요함
                     e.printStackTrace();
                 }
             },
