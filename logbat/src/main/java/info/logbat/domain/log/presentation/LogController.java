5b0f47af72ff53db6d44ff93faffa69527c0a7cc
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-21 19:36:13 +0900

    refactor: saveLogs API 리팩토링
    - List로 전달받은 요청을 처리하는 API로 리팩토링했습니다.
    - 이에 따른 테스트 수정을 진행했습니다.
    - Deprecated된 메서드를 제거했습니다.

--- logbat/src/main/java/info/logbat/domain/log/presentation/LogController.java
+++ logbat/src/main/java/info/logbat/domain/log/presentation/LogController.java
@@ -1,18 +1,19 @@
 package info.logbat.domain.log.presentation;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
 import info.logbat.dev.aop.CountTest;
 import info.logbat.domain.log.application.LogService;
-import info.logbat.domain.log.application.payload.request.CreateLogServiceRequest;
 import info.logbat.domain.log.presentation.payload.request.CreateLogRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,19 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class LogController {
 
-  private final LogService logService;
+    private final LogService logService;
 
-  @CountTest
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public void saveLog(
-      @RequestHeader("appKey")
-      @NotBlank(message = "appKey가 비어있습니다.") String appKey,
-
-      @Valid @RequestBody CreateLogRequest request
-  ) {
-
-    logService.saveLog(CreateLogServiceRequest.of(appKey, request));
-  }
+    @CountTest
+    @PostMapping
+    public ResponseEntity<Void> saveLogs(
+        @RequestHeader("App-Key") @NotBlank(message = "appKey가 비어있습니다.") String appKey,
+        @Valid @RequestBody List<CreateLogRequest> request) {
+        logService.saveLogs(appKey, request);
+        return ResponseEntity.status(CREATED).build();
+    }
 
 }
