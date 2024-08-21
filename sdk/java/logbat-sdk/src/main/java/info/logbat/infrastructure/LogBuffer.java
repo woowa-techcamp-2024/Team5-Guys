c1f157d54bee4ff6fb6536c566832d4247b87f89
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-21 09:59:17 +0900

    feat: LinkedBlockingQueue를 관리하는 LogBuffer 추가

--- sdk/java/logbat-sdk/src/main/java/info/logbat/infrastructure/LogBuffer.java
+++ sdk/java/logbat-sdk/src/main/java/info/logbat/infrastructure/LogBuffer.java
@@ -1,5 +1,25 @@
 package info.logbat.infrastructure;
 
+import info.logbat.domain.log.Log;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class LogBuffer {
 
+    private final LinkedBlockingQueue<Log> logQueue;
+
+    public void addLog(Log log) {
+        logQueue.add(log);
+    }
+
+    public List<Log> getLogs(int count) {
+        List<Log> logs = new ArrayList<>();
+        logQueue.drainTo(logs, count);
+        return logs;
+    }
+
+    public LogBuffer(LinkedBlockingQueue<Log> logQueue) {
+        this.logQueue = logQueue;
+    }
 }
