78368f34c2aaa744e0a635e61ac1e505823f88e0
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-21 23:02:38 +0900

    feat: timestamp 전송 시 DateTimeFormmater 적용

--- sdk/java/logbat-sdk/src/main/java/info/logbat/adapter/logback/LogbatAppender.java
+++ sdk/java/logbat-sdk/src/main/java/info/logbat/adapter/logback/LogbatAppender.java
@@ -8,10 +8,13 @@ import info.logbat.infrastructure.payload.LogSendRequest;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class LogbatAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
 
     private Logbat logbat;
+    private static final DateTimeFormatter CUSTOM_FORMATTER =
+        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
     @Override
     public void start() {
@@ -26,7 +29,8 @@ public class LogbatAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
         String timestamp = LocalDateTime.ofInstant(
             Instant.ofEpochMilli(eventObject.getTimeStamp()),
             ZoneId.systemDefault()
-        ).toString();
+        ).format(CUSTOM_FORMATTER);
+
         logbat.writeLog(new LogSendRequest(level, message, timestamp));
     }
 }
