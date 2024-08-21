5cadb55e83e51c544f43a7cec4bfd50bfc3c12e5
luizy <luizy991212@gmail.com>
2024-08-21 15:58:19 +0900

    feat: 로그 레벨 고도화
    - trace, debug, info, warn, error 추가

--- sdk/js/src/index.ts
+++ sdk/js/src/index.ts
@@ -1,7 +1,14 @@
 class LogBat {
     private static appKey: string = '';
     private static apiEndpoint: string = 'https://api.logbat.info/logs';
-    private static originalConsole: { log: typeof console.log; error: typeof console.error };
+    private static originalConsole: {
+        trace: typeof console.trace
+        debug: typeof console.debug
+        info: typeof console.info
+        log: typeof console.log
+        warn: typeof console.warn
+        error: typeof console.error
+    };
     private static isInitialized: boolean = false;
 
     public static init(config: { appKey: string }): void {
@@ -15,9 +22,33 @@ class LogBat {
         this.isInitialized = true;
     }
 
+    public static async trace(...args: any[]): Promise<void> {
+        const promise = this.sendLog('trace', args);
+        this.originalConsole.trace(...args);
+        await promise;
+    }
+
+    public static async debug(...args: any[]): Promise<void> {
+        const promise = this.sendLog('debug', args);
+        this.originalConsole.debug(...args);
+        await promise;
+    }
+
+    public static async info(...args: any[]): Promise<void> {
+        const promise = this.sendLog('info', args);
+        this.originalConsole.info(...args);
+        await promise;
+    }
+
     public static async log(...args: any[]): Promise<void> {
         const promise = this.sendLog('info', args);
         this.originalConsole.log(...args);
+        await promise;
+    }
+
+    public static async warn(...args: any[]): Promise<void> {
+        const promise = this.sendLog('warn', args);
+        this.originalConsole.warn(...args);
         await promise;
     }
 
@@ -56,12 +87,32 @@ class LogBat {
 
     private static overrideConsoleMethods(): void {
         this.originalConsole = {
+            trace: console.trace,
+            debug: console.debug,
+            info: console.info,
             log: console.log,
+            warn: console.warn,
             error: console.error
+        };
+
+        console.trace = (...args: any[]): void => {
+            this.trace(...args).catch(err => this.originalConsole.error('Error in LogBat trace:', err));
+        };
+
+        console.debug = (...args: any[]): void => {
+            this.debug(...args).catch(err => this.originalConsole.error('Error in LogBat debug:', err));
+        };
+
+        console.info = (...args: any[]): void => {
+            this.info(...args).catch(err => this.originalConsole.error('Error in LogBat info:', err));
         };
 
         console.log = (...args: any[]): void => {
             this.log(...args).catch(err => this.originalConsole.error('Error in LogBat log:', err));
+        };
+
+        console.warn = (...args: any[]): void => {
+            this.warn(...args).catch(err => this.originalConsole.error('Error in LogBat warn:', err));
         };
 
         console.error = (...args: any[]): void => {
