132b055b0942a55f975bbf444d36dc2199787636
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-22 12:09:20 +0900

    refactor: 메세지 생성 로직 메서드 분리를 통해 리팩토링

--- sdk/java/logbat-sdk/src/main/java/info/logbat/application/LogSendRequestFactory.java
+++ sdk/java/logbat-sdk/src/main/java/info/logbat/application/LogSendRequestFactory.java
@@ -2,13 +2,16 @@ package info.logbat.application;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
 import info.logbat.infrastructure.payload.LogSendRequest;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LogSendRequestFactory {
 
@@ -25,47 +28,54 @@ public class LogSendRequestFactory {
     /**
      * ILoggingEvent로부터 LogSendRequest를 생성하는 메서드
      */
+
     public LogSendRequest createLogSendRequest(ILoggingEvent eventObject) {
-
-        Level level = eventObject.getLevel();
-        String formattedLevel = getFormattedLevel(level);
-        String timestamp = LocalDateTime.ofInstant(
-            Instant.ofEpochMilli(eventObject.getTimeStamp()),
-            ZoneId.systemDefault()
-        ).format(CUSTOM_FORMATTER);
-
-        String threadName = eventObject.getThreadName();
-        String loggerName = eventObject.getLoggerName();
-        Map<String, String> mdcProperties = eventObject.getMDCPropertyMap();
-        String mdcString = mdcProperties.isEmpty() ? "" : " MDC: " + mdcProperties;
-
-        String throwableString = "";
-        if (eventObject.getThrowableProxy() != null) {
-            throwableString = " EXCEPTION: " + eventObject.getThrowableProxy().getMessage();
-        }
-
-        /**
-         * 로그 메시지를 형식화하여 생성합니다.
-         * 형식: [ThreadName] [LoggerName] Level - Message (MDC 정보) (예외 정보)
-         *
-         * MDC 정보와 예외 정보는 각각 존재하는 경우에만 메시지에 추가됩니다.
-         * 예시: [main] [com.example.MyClass] INFO - This is a test message MDC: {userId=12345} EXCEPTION: NullPointerException
-         */
-        String message = String.format(
-            "[%s] [%s] %s - %s%s%s",
-            threadName, loggerName, formattedLevel, eventObject.getFormattedMessage(),
-            mdcString, throwableString
+        return new LogSendRequest(
+            getFormattedLevel(eventObject.getLevel()),
+            getFormattedData(eventObject),
+            formatTimestamp(eventObject.getTimeStamp())
         );
+    }
 
-        return new LogSendRequest(formattedLevel, message, timestamp);
+    /**
+     * 로그 메시지를 형식화하여 생성합니다. 형식: [ThreadName] [LoggerName] Level - Message (MDC 정보) (예외 정보)
+     * <p>
+     * MDC 정보와 예외 정보는 각각 존재하는 경우에만 메시지에 추가됩니다. 예시: [main] [com.example.MyClass] INFO - This is a
+     * test message MDC: {userId=12345} EXCEPTION: NullPointerException
+     */
+    private String getFormattedData(ILoggingEvent eventObject) {
+        return String.format(
+            "[%s] [%s] %s - %s%s%s",
+            eventObject.getThreadName(),
+            eventObject.getLoggerName(),
+            getFormattedLevel(eventObject.getLevel()),
+            eventObject.getFormattedMessage(),
+            getMdcString(eventObject.getMDCPropertyMap()),
+            getExceptionString(eventObject.getThrowableProxy())
+        );
     }
 
     private String getFormattedLevel(Level level) {
-        return isSupportedLevel(level) ? level.toString() : OTHER_LEVEL;
+        return SUPPORTED_LEVELS.contains(level) ? level.toString() : OTHER_LEVEL;
     }
 
-    private boolean isSupportedLevel(Level level) {
-        return SUPPORTED_LEVELS.contains(level);
+    private String formatTimestamp(long timestamp) {
+        return LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(timestamp),
+            ZoneId.systemDefault()
+        ).format(CUSTOM_FORMATTER);
     }
 
+    private String getMdcString(Map<String, String> mdcProperties) {
+        return mdcProperties.isEmpty() ? "" :
+            " MDC: " + mdcProperties.entrySet().stream()
+                .map(entry -> entry.getKey() + "=" + entry.getValue())
+                .collect(Collectors.joining(", ", "{", "}"));
+    }
+
+    private String getExceptionString(IThrowableProxy throwableProxy) {
+        return Optional.ofNullable(throwableProxy)
+            .map(t -> " EXCEPTION: " + t.getMessage())
+            .orElse("");
+    }
 }
