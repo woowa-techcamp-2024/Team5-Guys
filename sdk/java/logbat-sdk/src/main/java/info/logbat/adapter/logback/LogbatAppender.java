c9f3b92d1bd221262c9d3a9ca45746d42cdb7740
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-22 10:24:45 +0900

    feat: Logbat 내부에서 ILoggingEvent를 의존해서 사용하도록 구현
    - Level을 Logbat 내부에서 지원하는 레벨로 제약 추가
    - 메세지 생성 로직을 내부로 캡슐화

--- sdk/java/logbat-sdk/src/main/java/info/logbat/adapter/logback/LogbatAppender.java
+++ sdk/java/logbat-sdk/src/main/java/info/logbat/adapter/logback/LogbatAppender.java
@@ -4,17 +4,10 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
 import info.logbat.LogbatFactory;
 import info.logbat.application.Logbat;
-import info.logbat.infrastructure.payload.LogSendRequest;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 
 public class LogbatAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
 
     private Logbat logbat;
-    private static final DateTimeFormatter CUSTOM_FORMATTER =
-        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
     @Override
     public void start() {
@@ -24,13 +17,6 @@ public class LogbatAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
 
     @Override
     protected void append(ILoggingEvent eventObject) {
-        String level = eventObject.getLevel().toString();
-        String message = eventObject.getFormattedMessage();
-        String timestamp = LocalDateTime.ofInstant(
-            Instant.ofEpochMilli(eventObject.getTimeStamp()),
-            ZoneId.systemDefault()
-        ).format(CUSTOM_FORMATTER);
-
-        logbat.writeLog(new LogSendRequest(level, message, timestamp));
+        logbat.writeLog(eventObject);
     }
 }
--- sdk/java/logbat-sdk/src/main/java/info/logbat/application/Logbat.java
+++ sdk/java/logbat-sdk/src/main/java/info/logbat/application/Logbat.java
@@ -1,14 +1,71 @@
 package info.logbat.application;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import info.logbat.infrastructure.AsyncLogWriter;
 import info.logbat.infrastructure.payload.LogSendRequest;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.Set;
 
 public class Logbat {
 
     private final AsyncLogWriter asyncLogWriter;
 
-    public void writeLog(LogSendRequest log) {
-        asyncLogWriter.sendLog(log);
+    private static final DateTimeFormatter CUSTOM_FORMATTER =
+        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+    // 로그 레벨 집합을 미리 정의
+    private static final Set<Level> SUPPORTED_LEVELS = Set.of(
+        Level.TRACE, Level.DEBUG, Level.INFO, Level.WARN, Level.ERROR
+    );
+
+    public void writeLog(ILoggingEvent eventObject) {
+        Level level = eventObject.getLevel();
+
+        /**
+         * 로그 레벨이 SUPPORTED_LEVELS에 포함되어 있는 경우에만 로그 전송
+         */
+        if (SUPPORTED_LEVELS.contains(level)) {
+            String formattedLevel = level.toString();
+            String timestamp = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(eventObject.getTimeStamp()),
+                ZoneId.systemDefault()
+            ).format(CUSTOM_FORMATTER);
+
+            /**
+             * thread 이름, logger 이름, MDC 정보를 문자열로 변환
+             */
+            String threadName = eventObject.getThreadName();
+            String loggerName = eventObject.getLoggerName();
+            Map<String, String> mdcProperties = eventObject.getMDCPropertyMap();
+            String mdcString = mdcProperties.isEmpty() ? "" : " MDC: " + mdcProperties;
+
+            /**
+             * throwable 정보가 있는 경우 throwable 정보를 문자열로 변환
+             */
+            String throwableString = "";
+            if (eventObject.getThrowableProxy() != null) {
+                throwableString = " EXCEPTION: " + eventObject.getThrowableProxy().getMessage();
+            }
+
+            /**
+             * thread 이름과 logger 이름을 포함한 로그 메시지 생성
+             */
+            String message = String.format(
+                "[%s] [%s] %s - %s%s%s",
+                threadName, loggerName, formattedLevel, eventObject.getFormattedMessage(),
+                mdcString, throwableString
+            );
+
+            /**
+             * LogSendRequest 객체 생성 및 로그 전송
+             */
+            asyncLogWriter.sendLog(new LogSendRequest(formattedLevel, message, timestamp));
+        }
     }
 
     public Logbat(AsyncLogWriter asyncLogWriter) {
