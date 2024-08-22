346d48550c474ac7e45654437191f46cc8b89503
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-22 10:37:43 +0900

    feat: Logbat에서 처리 불가능한 예외는 DEBUG로 설정하여 전송하도록 리팩토링함

--- sdk/java/logbat-sdk/src/main/java/info/logbat/application/LogSendRequestFactory.java
+++ sdk/java/logbat-sdk/src/main/java/info/logbat/application/LogSendRequestFactory.java
@@ -1,5 +1,6 @@
 package info.logbat.application;
 
+import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import info.logbat.infrastructure.payload.LogSendRequest;
 import java.time.Instant;
@@ -7,17 +8,27 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.Set;
 
 public class LogSendRequestFactory {
 
     private static final DateTimeFormatter CUSTOM_FORMATTER =
         DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+    /**
+     * Logbat에서 기본적으로 지원하는 로그 레벨
+     */
+    private static final Set<Level> SUPPORTED_LEVELS = Set.of(
+        Level.TRACE, Level.DEBUG, Level.INFO, Level.WARN, Level.ERROR
+    );
+    private static final String OTHER_LEVEL = "DEBUG";
 
     /**
      * ILoggingEvent로부터 LogSendRequest를 생성하는 메서드
      */
     public LogSendRequest createLogSendRequest(ILoggingEvent eventObject) {
-        String formattedLevel = eventObject.getLevel().toString();
+
+        Level level = eventObject.getLevel();
+        String formattedLevel = getFormattedLevel(level);
         String timestamp = LocalDateTime.ofInstant(
             Instant.ofEpochMilli(eventObject.getTimeStamp()),
             ZoneId.systemDefault()
@@ -41,4 +52,13 @@ public class LogSendRequestFactory {
 
         return new LogSendRequest(formattedLevel, message, timestamp);
     }
+
+    private String getFormattedLevel(Level level) {
+        return isSupportedLevel(level) ? level.toString() : OTHER_LEVEL;
+    }
+
+    private boolean isSupportedLevel(Level level) {
+        return SUPPORTED_LEVELS.contains(level);
+    }
+
 }
--- sdk/java/logbat-sdk/src/main/java/info/logbat/application/Logbat.java
+++ sdk/java/logbat-sdk/src/main/java/info/logbat/application/Logbat.java
@@ -1,40 +1,20 @@
 package info.logbat.application;
 
-import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import info.logbat.infrastructure.AsyncLogWriter;
 import info.logbat.infrastructure.payload.LogSendRequest;
-import java.time.format.DateTimeFormatter;
-import java.util.Set;
 
 public class Logbat {
 
     private final AsyncLogWriter asyncLogWriter;
     private final LogSendRequestFactory logSendRequestFactory;
 
-    private static final DateTimeFormatter CUSTOM_FORMATTER =
-        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-
-    // 로그 레벨 집합을 미리 정의
-    private static final Set<Level> SUPPORTED_LEVELS = Set.of(
-        Level.TRACE, Level.DEBUG, Level.INFO, Level.WARN, Level.ERROR
-    );
 
     public void writeLog(ILoggingEvent eventObject) {
-        Level level = eventObject.getLevel();
-
-        /**
-         * 로그 레벨이 SUPPORTED_LEVELS에 포함되어 있는 경우에만 로그 전송
-         */
-        if (isSupportedLevel(level)) {
-            LogSendRequest logSendRequest = logSendRequestFactory.createLogSendRequest(eventObject);
-            asyncLogWriter.sendLog(logSendRequest);
-        }
+        LogSendRequest logSendRequest = logSendRequestFactory.createLogSendRequest(eventObject);
+        asyncLogWriter.sendLog(logSendRequest);
     }
 
-    private boolean isSupportedLevel(Level level) {
-        return SUPPORTED_LEVELS.contains(level);
-    }
 
     public Logbat(AsyncLogWriter asyncLogWriter, LogSendRequestFactory logSendRequestFactory) {
         this.asyncLogWriter = asyncLogWriter;
