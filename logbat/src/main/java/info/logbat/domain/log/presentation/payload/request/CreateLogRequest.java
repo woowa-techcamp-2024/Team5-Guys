a7977167a2c73e249fdb159455606ebfd8f13d69
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-22 10:08:22 +0900

    refactor: 로그 JSON 데이터 검증 변경
    - data에 대한 `@NotBlank` -> `@NotNull`
      - 공백 로그를 남길 수 있다는 엣지 케이스 고려

--- logbat/src/main/java/info/logbat/domain/log/presentation/payload/request/CreateLogRequest.java
+++ logbat/src/main/java/info/logbat/domain/log/presentation/payload/request/CreateLogRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 public record CreateLogRequest(@NotBlank(message = "로그 레벨이 비어있습니다.") String level,
-                               @NotBlank(message = "로그 데이터가 비어있습니다.") String data,
+                               @NotNull(message = "로그 데이터가 비어있습니다.") String data,
                                @NotNull(message = "타임스탬프가 비어있습니다.") LocalDateTime timestamp) {
 
     public Log toEntity(Long appId) {
