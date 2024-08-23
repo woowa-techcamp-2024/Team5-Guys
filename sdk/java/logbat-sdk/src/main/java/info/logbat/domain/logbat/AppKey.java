f95c1360d046b17417046dd4ed04d0380e126e16
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-23 23:53:01 +0900

    docs: AppKey 주석 작성
    - AppKey에 대한 주석을 작성했습니다.

--- sdk/java/logbat-sdk/src/main/java/info/logbat/domain/logbat/AppKey.java
+++ sdk/java/logbat-sdk/src/main/java/info/logbat/domain/logbat/AppKey.java
@@ -3,16 +3,38 @@ package info.logbat.domain.logbat;
 import info.logbat.exception.InvalidOptionException;
 import java.util.UUID;
 
+/**
+ * AppKey represents a unique identifier for an application.
+ * <p>
+ * AppKey is a value object that holds a UUID string as the unique identifier for an application. It
+ * used to identify an application in the LogBat system.
+ * </p>
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
 public class AppKey {
 
     private final String value;
 
+    /**
+     * Constructs an AppKey object with the given UUID string.
+     *
+     * @param value the UUID string to be used as the AppKey
+     * @throws InvalidOptionException if the given UUID string is null, empty, or not a valid UUID
+     *                                string
+     */
     public AppKey(String value) throws InvalidOptionException {
         validateAppKey(value);
         validateUUIDString(value);
         this.value = value;
     }
 
+    /**
+     * Returns the UUID string value of the AppKey.
+     *
+     * @return the UUID string value of the AppKey
+     */
     public String getValue() {
         return value;
     }
