d21c9a56d283b1c2717990d220f0c0927102516f
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-25 15:36:00 +0900

    refactor: API 전달 형식 변경
    - Body -> Path Parameter

--- logbat/src/main/java/info/logbat/domain/project/presentation/payload/request/AppCacheRemoveRequest.java
+++
@@ -1,7 +0,0 @@
-package info.logbat.domain.project.presentation.payload.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record AppCacheRemoveRequest(@NotBlank(message = "토큰은 Null일 수 없습니다.") String token) {
-
-}
