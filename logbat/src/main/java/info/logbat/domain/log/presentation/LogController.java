ee6e3a1fb2eac8bae9ec497e92112acfa61c916b
luizy <luizy991212@gmail.com>
2024-08-27 09:53:45 +0900

    refactor: Controller 빈 배열 검사 추가

--- logbat/src/main/java/info/logbat/domain/log/presentation/LogController.java
+++ logbat/src/main/java/info/logbat/domain/log/presentation/LogController.java
@@ -3,17 +3,13 @@ package info.logbat.domain.log.presentation;
 import info.logbat.dev.aop.CountTest;
 import info.logbat.domain.log.application.LogService;
 import info.logbat.domain.log.presentation.payload.request.CreateLogRequest;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import java.util.List;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/logs")
@@ -26,8 +22,8 @@ public class LogController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void saveLogs(
-        @RequestHeader("App-Key") @NotBlank(message = "appKey가 비어있습니다.") String appKey,
-        @RequestBody @Valid List<CreateLogRequest> requests) {
+            @RequestHeader("App-Key") @NotBlank(message = "appKey가 비어있습니다.") String appKey,
+            @RequestBody @NotEmpty List<CreateLogRequest> requests) {
         logService.saveLogs(appKey, requests);
     }
 
