1ddfcee997a08ce967aac9c710417881a326933b
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-27 10:07:36 +0900

    style: LogController
    - 린트 수정

--- logbat/src/main/java/info/logbat/domain/log/presentation/LogController.java
+++ logbat/src/main/java/info/logbat/domain/log/presentation/LogController.java
@@ -5,11 +5,15 @@ import info.logbat.domain.log.application.LogService;
 import info.logbat.domain.log.presentation.payload.request.CreateLogRequest;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/logs")
@@ -22,8 +26,8 @@ public class LogController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void saveLogs(
-            @RequestHeader("App-Key") @NotBlank(message = "appKey가 비어있습니다.") String appKey,
-            @RequestBody @NotEmpty List<CreateLogRequest> requests) {
+        @RequestHeader("App-Key") @NotBlank(message = "appKey가 비어있습니다.") String appKey,
+        @RequestBody @NotEmpty List<CreateLogRequest> requests) {
         logService.saveLogs(appKey, requests);
     }
 
