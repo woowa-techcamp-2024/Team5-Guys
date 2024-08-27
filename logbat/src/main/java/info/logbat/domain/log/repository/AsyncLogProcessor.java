e7da5606b9f3268eb9038379b8d3a154bcb8deeb
KyungMin Lee <hello.tidavid@gmail.com>
2024-08-28 00:22:07 +0900

    chore: Java-Doc및 사용하지 않는 메서드 Deprecated 처리
    - 사용하지 않는 메서드에 대한 Deprecated 처리를 진행했습니다.
    - AsyncLogProcessor에 대한 JavaDoc를 작성했습니다.

--- logbat/src/main/java/info/logbat/domain/log/repository/AsyncLogProcessor.java
+++ logbat/src/main/java/info/logbat/domain/log/repository/AsyncLogProcessor.java
@@ -12,24 +12,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-
 /**
- * 비동기식 로그 처리를 담당하는 것은 AsyncLogProcessor입니다. 리더-팔로워 패턴을 사용하여 로그 항목을 처리하고 대량으로 저장합니다. 리더는 로그 큐에서 로그
- * 항목을 가져와 팔로워 스레드 풀에 전달합니다. 팔로워는 로그 항목을 대량으로 저장합니다.
+ * 비동기적으로 로그를 처리하는 클래스입니다. 이 클래스는 로그를 저장하는 비동기 작업을 수행하며, 이를 위해 별도의 스레드 풀을 사용합니다.
  */
 @Slf4j
 @Component
 public class AsyncLogProcessor {
 
+    // 로그 저장 작업을 수행하는 스레드 풀
     private final ExecutorService followerExecutor;
+    // 로그를 소비하는 Consumer 객체
     private final info.logbat.domain.log.queue.Consumer<Log> consumer;
 
     /**
-     * 지정된 시간 제한, 일괄 크기 및 JdbcTemplate을 사용하여 AsyncLogProcessor를 구축합니다.
+     * 지정된 Consumer 객체와 JdbcTemplate을 사용하여 새 AsyncLogProcessor를 생성합니다. HikariDataSource의 최대 풀 크기의
+     * 50%를 사용하여 스레드 풀을 초기화합니다.
      *
-     * @param timeout      팔로워 스레드가 대기하는 시간 제한
-     * @param bulkSize     팔로워 스레드가 한 번에 처리하는 로그 항목 수
-     * @param jdbcTemplate JdbcTemplate
+     * @param consumer     로그를 소비하는 Consumer 객체
+     * @param jdbcTemplate JdbcTemplate 객체
      */
     public AsyncLogProcessor(info.logbat.domain.log.queue.Consumer<Log> consumer,
         JdbcTemplate jdbcTemplate) {
@@ -46,47 +46,33 @@ public class AsyncLogProcessor {
     }
 
     /**
-     * 비동기식 로그 처리를 시작합니다. 리더 스레드를 시작하고 로그 저장 함수를 전달합니다.
+     * 로그 처리를 초기화하고 시작합니다. 리더 태스크를 비동기적으로 실행합니다.
      *
-     * @param saveFunction 로그 저장 함수
+     * @param saveFunction 로그를 저장하는 함수
      */
     public void init(Consumer<List<Log>> saveFunction) {
         CompletableFuture.runAsync(() -> leaderTask(saveFunction));
     }
 
     /**
-     * 로그를 제출합니다.
-     * <p>
-     * 로그 큐에 로그를 추가합니다.
+     * 리더 태스크를 실행하는 private 메서드입니다. 로그를 소비하고 팔로워 스레드 풀에 저장 작업을 제출합니다.
      *
-     * @param log 로그
-     */
-    public void submitLog(Log log) {
-        // Queue 크기를 제한할 거면 offer 사용하도록 변경
-//        logQueue.add(log);
-    }
-
-    /**
-     * 로그 리스트를 제출합니다.
-     *
-     * @param logs 로그 리스트
-     */
-    public void submitLogs(List<Log> logs) {
-//        logQueue.addAll(logs);
-    }
-
-    /**
-     * 리더 스레드를 시작합니다. 로그를 저장하는 함수를 전달합니다.
-     * <p>
-     * 리더 스레드는 로그 큐에서 로그 항목을 가져와 팔로워 스레드 풀에 전달합니다. 팔로워는 로그 항목을 대량으로 저장합니다. 리더 스레드는 종료되지 않는 한 계속해서
-     * 로그를 처리합니다. 리더 스레드가 종료되면 팔로워 스레드 풀도 종료됩니다.
-     *
-     * @param saveFunction 로그 저장 함수
+     * @param saveFunction 로그를 저장하는 함수
      */
     private void leaderTask(Consumer<List<Log>> saveFunction) {
         while (!Thread.currentThread().isInterrupted()) {
             List<Log> logs = consumer.consume();
             followerExecutor.execute(() -> saveFunction.accept(logs));
         }
+    }
+
+    @Deprecated(forRemoval = true)
+    public void submitLog(Log log) {
+        throw new UnsupportedOperationException("This method is deprecated");
+    }
+
+    @Deprecated(forRemoval = true)
+    public void submitLogs(List<Log> logs) {
+        throw new UnsupportedOperationException("This method is deprecated");
     }
 }
