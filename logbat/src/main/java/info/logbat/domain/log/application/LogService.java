1ee256459f728b7c6327531837df94f702e67a2b
luizy <luizy991212@gmail.com>
2024-08-27 10:11:17 +0900

    fix: 형식에 맞는 값을 제거하고 배열 저장

--- logbat/src/main/java/info/logbat/domain/log/application/LogService.java
+++ logbat/src/main/java/info/logbat/domain/log/application/LogService.java
@@ -8,8 +8,10 @@ import info.logbat.domain.project.application.AppService;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class LogService {
@@ -20,7 +22,14 @@ public class LogService {
     public void saveLogs(String appKey, List<CreateLogRequest> requests) {
         Long appId = appService.getAppIdByToken(appKey);
         List<Log> logs = new ArrayList<>(requests.size());
-        requests.forEach(request -> logs.add(request.toEntity(appId)));
+        requests.forEach(request -> {
+            try {
+                logs.add(request.toEntity(appId));
+            }
+            catch (Exception e) {
+                log.error("Failed to convert request to entity: {}", request, e);
+            }
+        });
         logRepository.saveAll(logs);
     }
 
