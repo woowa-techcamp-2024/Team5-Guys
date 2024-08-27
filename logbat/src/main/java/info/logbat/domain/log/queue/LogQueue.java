c7a94acd95e39cbfc1a15c63f89265c830258bbe
Kim Minju <111269144+miiiinju1@users.noreply.github.com>
2024-08-27 21:29:35 +0900

    refactor: LogQueue 동작 로직 수정
    - LockSupports를 활용한 Thread lock방식을 적용했습니다.
    - `volatile Thread consumerThread`에 대한 VarHandle을 적용했습니다.

--- logbat/src/main/java/info/logbat/domain/log/queue/LogQueue.java
+++ logbat/src/main/java/info/logbat/domain/log/queue/LogQueue.java
@@ -1,34 +1,46 @@
 package info.logbat.domain.log.queue;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.LockSupport;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class LogQueue<T> implements Producer<T>, Consumer<T> {
 
-    private final LinkedList<T> queue = new LinkedList<>();
-    private final long timeout;
-    private final int bulkSize;
+    private static final VarHandle CONSUMER_THREADS;
 
+    static {
+        try {
+            CONSUMER_THREADS = MethodHandles.lookup()
+                .findVarHandle(LogQueue.class, "consumerThread", Thread.class);
+        } catch (ReflectiveOperationException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    private final LinkedList<T> queue = new LinkedList<>();
+    private final long timeoutNanos;
+    private final int bulkSize;
     private volatile Thread consumerThread;
 
-    public LogQueue(@Value("${jdbc.async.timeout}") Long timeout,
+
+    public LogQueue(@Value("${jdbc.async.timeout}") Long timeoutMillis,
         @Value("${jdbc.async.bulk-size}") Integer bulkSize) {
-        this.timeout = timeout;
+        this.timeoutNanos = timeoutMillis * 1_000_000; // Convert to nanoseconds
         this.bulkSize = bulkSize;
     }
 
-    /*
-     * Consumer should be one thread
-     */
     @Override
     public List<T> consume() {
-        List<T> result = new ArrayList<>();
+        List<T> result = new ArrayList<>(bulkSize);
 
-        // Case1: Full Flush
         if (queue.size() >= bulkSize) {
             for (int i = 0; i < bulkSize; i++) {
                 result.add(queue.poll());
@@ -36,37 +48,29 @@ public class LogQueue<T> implements Producer<T>, Consumer<T> {
             return result;
         }
 
-        // Else Case: Blocking
-        // Blocked while Queue is Not Empty
+        Thread current = Thread.currentThread();
+        CONSUMER_THREADS.set(this, current);
+
         do {
-            try {
-                consumerThread = Thread.currentThread();
-                consumerThread.sleep(timeout);
-            } catch (InterruptedException e) {
-                // ignore
-            }
+            LockSupport.parkNanos(timeoutNanos);
         } while (queue.isEmpty());
 
-        // Bulk Size 만큼 꺼내서 반환
         for (int i = 0; i < bulkSize; i++) {
             result.add(queue.poll());
             if (queue.isEmpty()) {
                 break;
             }
         }
+
+        CONSUMER_THREADS.weakCompareAndSet(this, current, null);
         return result;
     }
 
-    /*
-     * Producer should be one thread
-     */
     @Override
     public void produce(List<T> data) {
         queue.addAll(data);
-        if (queue.size() >= bulkSize) {
-            if (consumerThread != null) {
-                consumerThread.interrupt();
-            }
+        if (consumerThread != null && queue.size() >= bulkSize) {
+            LockSupport.unpark(consumerThread);
         }
     }
 }
