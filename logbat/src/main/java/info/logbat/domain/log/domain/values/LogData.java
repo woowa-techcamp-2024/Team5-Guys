ff2722bd75faf1aba8584320fdf1fc1429d1f2da
luizy <luizy991212@gmail.com>
2024-08-27 09:52:33 +0900

    refactor: Log data는 비어있을 수 있다

--- logbat/src/main/java/info/logbat/domain/log/domain/values/LogData.java
+++ logbat/src/main/java/info/logbat/domain/log/domain/values/LogData.java
@@ -17,8 +17,8 @@ public class LogData {
   }
 
   private void validateData(String data) {
-    if (data == null || data.isBlank()) {
-      throw new IllegalArgumentException("log data는 null이거나 빈 문자열일 수 없습니다.");
+    if (data == null) {
+      throw new IllegalArgumentException("log data는 null일 수 없습니다.");
     }
   }
 }
