10cf89a9499621051b9d8cae8fd6648823196016
CodingLuizy <104267255+LuizyHub@users.noreply.github.com>
2024-08-29 11:20:35 +0900

    Fis/#139 LinkedList 자료구조 개선 (#141)
    * feat: single linked list 추가
    
    - 생산자 소비자가 병렬적으로 호출할 수 있는  single linked list 추가
    
    * feat: SingleLinkLogQueue 추가
    
    - SingleLinkedList를 활용한 LogQueue 추가

---
+++ logbat/src/main/java/info/logbat/domain/log/queue/SingleLinkLogQueue.java
@@ -0,0 +1,56 @@
+package info.logbat.domain.log.queue;
+
+import info.logbat.common.event.EventConsumer;
+import info.logbat.common.event.EventProducer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class SingleLinkLogQueue<T> implements EventProducer<T>, EventConsumer<T> {
+
+    private final SingleLinkedList<T> queue = new SingleLinkedList<>();
+    private final long timeout;
+    private final int bulkSize;
+
+    public SingleLinkLogQueue(@Value("${jdbc.async.timeout}") Long timeout,
+                              @Value("${jdbc.async.bulk-size}") Integer bulkSize) {
+        this.timeout = timeout;
+        this.bulkSize = bulkSize;
+    }
+
+    /*
+     * Consumer should be one thread
+     */
+    @Override
+    public List<T> consume() {
+        List<T> result = new ArrayList<>(bulkSize);
+
+        final long endTime = System.currentTimeMillis() + timeout;
+
+        while (result.isEmpty()) {
+            T data = queue.poll();
+            if (data != null) {
+                result.add(data);
+            }
+            if (result.size() >= bulkSize) {
+                break;
+            }
+            if (System.currentTimeMillis() >= endTime) {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    /*
+     * Producer should be one thread
+     */
+    @Override
+    public void produce(List<T> data) {
+        queue.addAll(data);
+    }
+}
